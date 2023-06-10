116050c4cbc1dc11e83a895c65d77cff6bb32960
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-06-10 16:01:37 +0200

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    error::Error,
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
@@ -10,18 +11,21 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         
-        handle_connection(stream);
+        let _ = handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let request_line = match buf_reader.lines().next() {
+        Some(request_line) => request_line?,
+        None => return Err("no request line".into()),
+    };
     
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "html/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "html/404.html")
     };
     
     let contents = fs::read_to_string(filename).unwrap();
@@ -30,4 +34,5 @@ fn handle_connection(mut stream: TcpStream) {
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     
     stream.write_all(response.as_bytes()).unwrap();
+    return Ok(());
 }
